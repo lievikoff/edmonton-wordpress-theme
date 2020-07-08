60d2684be90e634510d58921231f2e77a9ba6cd3
DmytroRaiko <55841333+DmytroRaiko@users.noreply.github.com>
2020-07-08 15:43:23 +0300

    change block-class 'alt-navigation-menu' to 'alt-navigation-social-media'

--- header.php
+++ header.php
@@ -137,40 +137,19 @@
 
 			</div>
 
-			<div class="alt-navigation-menu">
+			<div class="alt-navigation-social-media">
 
-				<?php if ( has_nav_menu( 'social' ) ) { ?>
+				
 
-				<nav aria-label="<?php esc_attr_e( 'Expanded Social links', 'edmonton' ); ?>" role="navigation">
-					<ul class="social-menu reset-list-style social-icons fill-children-current-color">
-
-						<?php
-						wp_nav_menu(
-							array(
-								'theme_location'  => 'social',
-								'container'       => '',
-								'container_class' => '',
-								'items_wrap'      => '%3$s',
-								'menu_id'         => '',
-								'menu_class'      => '',
-								'depth'           => 1,
-								'link_before'     => '<span class="screen-reader-text">',
-								'link_after'      => '</span>',
-								'fallback_cb'     => '',
-							)
-						);
-						?>
-
-					</ul>
-				</nav><!-- .social-menu -->
-
-				<?php } 
+				<?php 
 					if ( true === $enable_header_search ) {
 						get_template_part( 'template-parts/modal-search' );
 					}
+
 				?>
 
 			</div>
+			
 
 		</div>
 		<?php
