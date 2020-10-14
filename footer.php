c5b57eab896db5c5156a2b5a14a0c7f1fca5d342
Victoria Belova <victoriabelova@Victorias-MacBook-Pro.local>
2020-10-14 16:00:33 +0300

    =working on the footer, mobile view

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
  * @since Edmonton 1.0
  */
 
-?>		
+?>
 		<div id="footer-container">
 
 			<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
@@ -18,9 +18,9 @@
 			<footer id="site-footer" role="contentinfo" class="header-footer-group">
 
 				<div id="footer-user-text" class="section-inner">
-					
+
 					<p class="footer-copyright footer-user-text">
-						
+
 						<?php
 						if ( get_theme_mod( 'footer_text' ) ) {
 							echo get_theme_mod( 'footer_text' );
@@ -31,21 +31,24 @@
 
 					<div class="footer-credits">
 
-						<p class="footer-copyright">&copy;
-							<?php
-							echo date_i18n(
-								/* translators: Copyright date format, see https://www.php.net/date */
-								_x( 'Y', 'copyright date format', 'edmonton' )
-							);
-							?>
-							<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
-						</p><!-- .footer-copyright -->
+						<div class="footer-inner-wrap">
+							<p class="footer-copyright">&copy;
+								<?php
+								echo date_i18n(
+									/* translators: Copyright date format, see https://www.php.net/date */
+									_x( 'Y', 'copyright date format', 'edmonton' )
+								);
+								?>
+								<a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+							</p><!-- .footer-copyright -->
 
-						<p class="powered-by-edmonton">
-							<a href="<?php echo esc_url( __( '#', 'edmonton' ) ); ?>">
-								<?php echo __( 'by Chapak. ', 'edmonton' ); ?>
-							</a>
-						</p><!-- .powered-by-edmonton -->
+							<p class="powered-by-edmonton">
+								<a href="<?php echo esc_url( __( '#', 'edmonton' ) ); ?>">
+									<?php echo __( 'by Chapak. ', 'edmonton' ); ?>
+								</a>
+							</p><!-- .powered-by-edmonton -->
+						</div>
+
 
 						<p class="after-powered-by-edmonton">
 							<?php _e( 'All Right Reserved.', 'edmonton' ); ?>
